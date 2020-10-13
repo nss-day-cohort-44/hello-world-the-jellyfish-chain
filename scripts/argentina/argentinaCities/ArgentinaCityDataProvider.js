0066a12fabe72f016c400854221d0fa8fda54e25
Jasmin Kaset <kasetski@gmail.com>
2020-10-13 13:23:53 -0500

    fixed function in city list js

--- scripts/argentina/argentinaCities/ArgentinaCityDataProvider.js
+++ scripts/argentina/argentinaCities/ArgentinaCityDataProvider.js
@@ -14,5 +14,5 @@ const cities = [
 ]
 
 export const useCities = () => {
-  return [cities]
+  return cities
 }
--- scripts/argentina/argentinaCities/ArgentinaCityList.js
+++ scripts/argentina/argentinaCities/ArgentinaCityList.js
@@ -1,23 +1,27 @@
 import { useCities } from "./ArgentinaCityDataProvider.js"
 import { CityHTML } from "./ArgentinaCity.js"
 
-export const populateCityList = () => {
-  const CitiesList = useCities()
-  console.log(CitiesList)
-}
-
 // export const populateCityList = () => {
-
-//   const cityContainer = document.querySelector(".cities")
-//   const arrayOfCities = useCities()
-
-//   let buildCityList = "" 
-//   for (const singleCity of arrayOfCities) {
-//     buildCityList += CityHTML(singleCity)
-//   }
-
-//   cityContainer.innerHTML += `
-//   <div>${buildCityList}</div>
-//   `
-//   console.log("GOOD JOB! 👍")
+//   const CitiesList = useCities()
+//   console.log(CitiesList)
 // }
+
+export const populateCityList = () => {
+
+  const cityContainer = document.querySelector(".details__card--cities")
+  const arrayOfCities = useCities()
+  console.log(arrayOfCities)
+
+  let buildCityList = "" 
+  for (const singleCity of arrayOfCities) {
+    buildCityList += CityHTML(singleCity)
+  }
+
+ cityContainer.innerHTML += `
+  <section>
+  <div>${buildCityList}</div>
+  </section>
+  `
+console.log(cityContainer)
+  
+}
