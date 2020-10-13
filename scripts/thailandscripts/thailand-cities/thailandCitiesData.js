f9ed5f71081c33d08bc52421e86281578ebae913
negative1skunk@gmail.com <negative1skunk@gmail.com>
2020-10-13 10:24:02 -0500

    started on the cities data

---
+++ scripts/thailandscripts/thailand-cities/thailandCitiesData.js
@@ -0,0 +1,14 @@
+const citiesCollecton = [
+    {
+        name: "Bangkok"
+        population: "8.3 million"
+    },
+    {
+        name: Chiang Mai,
+        population: "130,000"
+    },
+    {
+        name: Pattaya,
+        population: "120,000"
+    }
+]
