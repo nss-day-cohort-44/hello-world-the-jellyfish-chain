d95b4c0ac42bcfe87870afe6cbb6c9c4bb8f494d
Jasmin Kaset <kasetski@gmail.com>
2020-10-13 11:35:55 -0500

    changed city list

--- scripts/argentina/argentinaCities/ArgentinaCityList.js
+++ scripts/argentina/argentinaCities/ArgentinaCityList.js
@@ -1,22 +1,19 @@
 import { useCities } from "./ArgentinaCityDataProvider.js"
-import { CityHTML } from "./ArgentinaCity"
+import { CityHTML } from "./ArgentinaCity.js"
 
-// a function that gets exported so main.js can execute it
+
 export const populateCityList = () => {
-  // we need to know what html element to reference to put cities in
+
   const cityContainer = document.querySelector(".cities")
-  // store what useCities returns into a variable (the array of copied cities)
   const arrayOfCities = useCities()
 
-  // starting the string to add all of the HTML to
   let buildCityList = "" 
-  // loop through the array of donuts, 
   for (const singleCity of arrayOfCities) {
-      // and turn each donut object into an html representation of a donut AND adding the HTML to our buildDonutList string
-      buildCityList += CityHTML(singleCity)
+    buildCityList += CityHTML(singleCity)
   }
 
-  // we need to put the list of donuts in the donutContainer
-  cityContainer.innerHTML += `<div class="coolStyles">${buildCityList}</div>`
+  cityContainer.innerHTML += `
+  <div>${buildCityList}</div>
+  `
   console.log("GOOD JOB! 👍")
 }
