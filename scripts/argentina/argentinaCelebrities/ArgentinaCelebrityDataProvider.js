22d79113fe66a8813bf25668116aefbb3c234293
Jasmin Kaset <kasetski@gmail.com>
2020-10-13 10:38:10 -0500

    updated data providers in all subdirectories, created export on ArgentinaCityDataProvider.js

---
+++ scripts/argentina/argentinaCelebrities/ArgentinaCelebrityDataProvider.js
@@ -0,0 +1,5 @@
+const celebrities = [
+  {name: "Pope Francis"},
+  {name: "Che Guevera"},
+  {name: "Jorge Luis Borges"}
+]
---
+++ scripts/argentina/argentinaCities/ArgentinaCityDataProvider.js
@@ -0,0 +1,9 @@
+const cities = [
+  {name: "Buenos Aries"},
+  {name: "Mendoza"},
+  {name: "Ushuaia"}
+]
+
+export const useCities = () => {
+  return cities.slice()
+}
