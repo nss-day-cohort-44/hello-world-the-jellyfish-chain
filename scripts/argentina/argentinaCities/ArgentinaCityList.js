75f67ef7d033b9f2c0f91df1569db4890856f57a
Jasmin Kaset <kasetski@gmail.com>
2020-10-13 13:56:07 -0500

    did something with citylist

--- scripts/argentina/argentinaCities/ArgentinaCityList.js
+++ scripts/argentina/argentinaCities/ArgentinaCityList.js
@@ -1,27 +1,19 @@
 import { useCities } from "./ArgentinaCityDataProvider.js"
 import { CityHTML } from "./ArgentinaCity.js"
 
-// export const populateCityList = () => {
-//   const CitiesList = useCities()
-//   console.log(CitiesList)
-// }
 
 export const populateCityList = () => {
 
-  const cityContainer = document.querySelector(".details__card--cities")
+  const cityContainer = document.querySelector(".cities")
   const arrayOfCities = useCities()
-  console.log(arrayOfCities)
 
   let buildCityList = "" 
   for (const singleCity of arrayOfCities) {
     buildCityList += CityHTML(singleCity)
   }
 
- cityContainer.innerHTML += `
-  <section>
+  cityContainer.innerHTML += `
   <div>${buildCityList}</div>
-  </section>
   `
-console.log(cityContainer)
   
 }
