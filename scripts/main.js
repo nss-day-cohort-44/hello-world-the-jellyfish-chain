0ee5bc7bff374cd4c231e25be0abc2c8bdde607c
ted <tedmarov@gmail.com>
2020-10-13 15:41:35 -0500

    Improved main for Iceland

--- scripts/main.js
+++ scripts/main.js
@@ -49,16 +49,11 @@ console.log("Welcome to the main module")
 
 
 
-import { useCeleb } from "./iceland/celebrities/icelandCelebrityDataProvider.js"
 import { celebList } from "./iceland/celebrities/icelandCelebrityList.js"
-import { useCity } from "./iceland/cities/icelandCityDataProvider.js"
 import { cityList } from "./iceland/cities/icelandCityList.js"
-import { useLandmark} from "./iceland/landmarks/icelandLandmarkDataProvider.js"
 import { landmarkList } from "./iceland/landmarks/icelandLandmarkList.js"
 
-const allTheCelebs = useCeleb()
-const allTheCities = useCity()
-const alltheLandmarks = useLandmark()
+
 celebList()
 cityList()
 landmarkList()
