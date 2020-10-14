548544789ed259998ee1fca02fa8cb58ab36580a
negative1skunk@gmail.com <negative1skunk@gmail.com>
2020-10-14 09:06:51 -0500

    updated css and a js linking

--- scripts/thailandscripts/thailand-celebs/thailandCeleb.js
+++ scripts/thailandscripts/thailand-celebs/thailandCeleb.js
@@ -1,6 +1,6 @@
 export const Celeb = (celebObj) => {
   return `
-     <p><span class="bold">Name</span>${celebObj.name}</p>
-     <p><span class="bold">Occupation</span>${celebObj.occupation}</p>
+     <p><span class="bold">Name</span>: ${celebObj.name}</p>
+     <p><span class="bold">Role</span>: ${celebObj.occupation}</p>
     `;
 };
